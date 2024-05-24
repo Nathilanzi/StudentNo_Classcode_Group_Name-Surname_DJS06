8b0415375443df588e6e6e3846c957dd3b4c24d2
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-05-24 08:54:06 +0200

    object matching

--- scripts.js
+++ scripts.js
@@ -32,3 +32,9 @@ console.log(Count of remaining provinces: ${noCapeProvinces.length});
 // 6. Finding 'S'
 const namesWithS = names.map(name => name.toLowerCase().includes('s'));
 console.log(namesWithS);
+// 7. Creating Object Mapping
+const nameProvinceMap = names.reduce((map, name, index) => {
+  map[name] = provinces[index];
+  return map;
+}, {});
+console.log(nameProvinceMap);
